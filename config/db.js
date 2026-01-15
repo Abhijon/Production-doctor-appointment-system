1189d7b0a25a5ce86fbde397a7e4dc6e55f81fc3
Abhijon <abhijonrupai2003@gmail.com>
2026-01-15 15:54:51 +0530

    Changed console

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose=require('mongoose')
-const colors=require('colors')
-const connectDB=async()=>{
+const mongoose = require('mongoose')
+const colors = require('colors')
+const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL)
-        console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
-        
+        console.log(`Mongodb got  connected ${mongoose.connection.host}`.bgGreen.white);
+
     } catch (error) {
-        console.log(`Mongodb server Issue ${error}` .bgRed.white)
-        
+        console.log(`Mongodb server Issue ${error}`.bgRed.white)
+
     }
 }
-module.exports=connectDB;
+module.exports = connectDB;
